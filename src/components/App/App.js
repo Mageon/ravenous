de2b20376203aa5575b7151a02cbfc64736458f5
Axel Quiñones <axelord@gmail.com>
2020-09-16 00:54:30 -0500

    First Part Completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div class="App">
+        <h1>Ravenous</h1>
+        <SearchBar />
+        <BusinessList />
     </div>
   );
 }
